ef4f955b20ed89e90a3614b6cd4638afd8cfa71c
Ubuntu <ubuntu@ip-172-31-38-181.ap-northeast-2.compute.internal>
2022-02-01 13:54:52 +0000

    add gal features to mention someone

--- commands/gal.js
+++ commands/gal.js
@@ -12,20 +12,26 @@ module.exports = {
 	    .reply(i18n.__mf("gal.howToUse"))
 	    .catch(console.error);
     }
-
-    const context = args.join(" "); //공백 허용
-    message.channel.send(i18n.__("gal.player")); //ko.json에 저장된 유저 멘션
+    
+    var context = args.join(" "); //공백 허용
+    var referContext = "";
+    if (context.indexOf("(") != -1 && context.indexOf(")") != -1 && context.indexOf("(") < context.indexOf(")")) {
+        referContext = context.substring(context.indexOf("(") + 1, context.indexOf(")"));
+	context = context.substring(0, context.indexOf("("));
+    }
     
     let galEmbed = new MessageEmbed()
       .setTitle(i18n.__mf("gal.embedTitle", {context: context}))  //공지 타이틀
-      //.setDescription(i18n.__("gal.embedDescription"))  //공지 설명
+      .setDescription(i18n.__mf("gal.embedDescription", {referContext: referContext}))  //공지 설명
       .setColor("#F8AA2A");
 
     galEmbed.setTimestamp(); //입력 시간 출력
 
     try {
+        message.channel.send(i18n.__("gal.player")); //ko.json에 저장된 유저 멘션
         galMessage = await message.channel.send(galEmbed);
 	//emoji 반응 추가
+	await message.delete();
         await galMessage.react("<:ri:925626746500431923>");
         await galMessage.react("<:gyo:925677426955145246>");
         await galMessage.react("<:chang:925626874242170931>");
@@ -35,6 +41,7 @@ module.exports = {
         await galMessage.react("<:tte:925626722873909308>");
         await galMessage.react("<:pi:925626916642390046>");
         await galMessage.react("<:seb:925626816247496775>");
+        await galMessage.react("<:rin:925627280515022848>");
     } catch (error) {
         console.error(error);
     }
